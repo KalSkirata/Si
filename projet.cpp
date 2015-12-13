d69abdbaf6426eaa5de5e78db245d319038a79fd
Lecocq Guillaume <KalSkirata@users.noreply.github.com>
2015-12-13 20:51:32 +0100

    documentation

--- projet.cpp
+++ projet.cpp
@@ -1,3 +1,9 @@
+/** 
+	\brief Remplace un tube de colle par un sabre laser sur une prise de caméra
+	\author Lecocq Guillaume
+	\file projet.cpp
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -10,9 +16,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 
-#include"glimage.hpp"
+#include"glimage/glimage.hpp"
 
 #define  GL_GLEXT_PROTOTYPES
+
+/**
+	\def définie si on affiche les différentes valeurs sur le terminal
+**/
 #define INFO if(0) printf
 
 #if defined(__APPLE__) || defined(MACOSX)
@@ -29,12 +39,17 @@ static void reshape         (int, int);
 static void drawBackground    (void);
 static void display         (void);
 
-static GLuint camText=0, hiltText = 0, bladeText = 0;
+static /** identifiant de la texture de la caméra **/ GLuint camText=0, 
+		 /** identifiant de la texture du manche du sabre laser **/ hiltText = 0,
+		 /** identifiant de la texture de la lame du sabre laser **/bladeText = 0;
 
+/** Capture de la caméra **/
 VideoCapture *capture;
 
+/** Matrice qui contiendra l'image original de la caméra **/
 Mat imgOriginal;
 
+/** Coordonnées (dans l'espace et de texture) du cube où la caméra sera texturée **/
 GLfloat camVBO[] = {
 	-1.f, -1.f, 0.f,
 	1.0f, 1.0f,
@@ -46,43 +61,62 @@ GLfloat camVBO[] = {
 	1.0f, 0.0f
 };
 
-GLfloat 	swordVBO[20], bladeVBO[32];
+GLfloat /** Coordonnées (dans l'espace et de texture) du cube où le manche sera texturé **/ hiltVBO[20], 
+		  /** Coordonnées (dans l'espace et de texture) du cube où la lame sera texturée **/ bladeVBO[32];
 
+/**
+	\func initialise les paramètres relatifs à OpenCV
+**/
 void initCV(){
 	capture = new VideoCapture(CV_CAP_ANY);
 }
 
+/**
+	\func initialise les positions de l'épée dans l'espace à l'endroit où l'on détecte le tube de colle
+	\param x coordonnées en x
+	\param y coordonnées en y
+	\param z coordonnées en z
+	\param width largeur
+	\param height hauteur
+**/
 void initSwordPosition(double x, double y, double z, double width, double height){
-	swordVBO[0] = x+0.2; swordVBO[1] = y; swordVBO[2] = z;
-	swordVBO[3] = 0.0; swordVBO[4] = 1.0;
+	//if(height > width){
+		hiltVBO[0] = x+0.2; hiltVBO[1] = y; hiltVBO[2] = z;
+		hiltVBO[3] = 0.0; hiltVBO[4] = 1.0;
 
-	swordVBO[5] = x+width+0.2; swordVBO[6] = y; swordVBO[7] = z;
-	swordVBO[8] = 0.0; swordVBO[9] = 0.0;
+		hiltVBO[5] = x+width+0.2; hiltVBO[6] = y; hiltVBO[7] = z;
+		hiltVBO[8] = 0.0; hiltVBO[9] = 0.0;
 
-	swordVBO[10] = x+width+0.2;	swordVBO[11] = y+height;	swordVBO[12] = z;
-	swordVBO[13] = 1.0;	swordVBO[14] = 0.0;
+		hiltVBO[10] = x+width+0.2;	hiltVBO[11] = y+height;	hiltVBO[12] = z;
+		hiltVBO[13] = 1.0;	hiltVBO[14] = 0.0;
 
-	swordVBO[15] = x+0.2;	swordVBO[16] = y+height;	swordVBO[17] = z;
-	swordVBO[18] = 1.0;	swordVBO[19] = 1.0;
+		hiltVBO[15] = x+0.2;	hiltVBO[16] = y+height;	hiltVBO[17] = z;
+		hiltVBO[18] = 1.0;	hiltVBO[19] = 1.0;
 
-	INFO("swordVBO : (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) \n", swordVBO[0], swordVBO[1], swordVBO[2], swordVBO[6], swordVBO[7], swordVBO[8], swordVBO[12], swordVBO[13], swordVBO[14], swordVBO[18], swordVBO[19]);
 	
-	bladeVBO[0] = x+0.2; bladeVBO[1] = y+height-0.1; bladeVBO[2] = z;
-	bladeVBO[3] = 0.0; bladeVBO[4] = 0.0;
+		bladeVBO[0] = x+0.2; bladeVBO[1] = y+height-0.1; bladeVBO[2] = z;
+		bladeVBO[3] = 0.0; bladeVBO[4] = 0.0;
 
-	bladeVBO[5] = x+width+0.2; bladeVBO[6] = y+height-0.1; bladeVBO[7] = z;
-	bladeVBO[8] = 1.0; bladeVBO[9] = 0.0;
+		bladeVBO[5] = x+width+0.2; bladeVBO[6] = y+height-0.1; bladeVBO[7] = z;
+		bladeVBO[8] = 1.0; bladeVBO[9] = 0.0;
 
-	bladeVBO[10] = x+width+0.2;	bladeVBO[11] = y+4*height;	bladeVBO[12] = z;
-	bladeVBO[13] = 1.0;	bladeVBO[14] = 1.0;
+		bladeVBO[10] = x+width+0.2;	bladeVBO[11] = y+4*height;	bladeVBO[12] = z;
+		bladeVBO[13] = 1.0;	bladeVBO[14] = 1.0;
 
-	bladeVBO[15] = x+0.2;	bladeVBO[16] = y+4*height;	bladeVBO[17] = z;
-	bladeVBO[18] = 0.0;	bladeVBO[19] = 1.0;
+		bladeVBO[15] = x+0.2;	bladeVBO[16] = y+4*height;	bladeVBO[17] = z;
+		bladeVBO[18] = 0.0;	bladeVBO[19] = 1.0;
+	//}
 
-	INFO("bladeVBO : (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) \n", bladeVBO[0], bladeVBO[1], bladeVBO[2], bladeVBO[6], bladeVBO[7], bladeVBO[8], bladeVBO[12], bladeVBO[13], bladeVBO[14], bladeVBO[18], bladeVBO[19]);
+	INFO("hiltVBO : (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) \n", hiltVBO[0], hiltVBO[1], hiltVBO[2], hiltVBO[5], hiltVBO[6], hiltVBO[7], hiltVBO[10], hiltVBO[11], hiltVBO[12], hiltVBO[15], hiltVBO[16], hiltVBO[17]);
+
+	INFO("bladeVBO : (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) (%f,%f,%f) \n", bladeVBO[0], bladeVBO[1], bladeVBO[2], bladeVBO[5], bladeVBO[6], bladeVBO[7], bladeVBO[10], bladeVBO[11], bladeVBO[12], hiltVBO[15], hiltVBO[16], hiltVBO[17]);
 }
 
-// Function turn a cv::Mat into a texture, and return the texture ID as a GLuint for use
+/**
+	\func transforme une matrice (ici l'image de la caméra) en une texture 2D
+	\param mat adresse de la matrice
+	\return ID de la texture 2D de la matrice
+**/
 GLuint matToTexture(cv::Mat &mat, GLenum minFilter, GLenum magFilter, GLenum wrapFilter)
 {
 	// Generate a number for our textureID's unique handle
@@ -144,26 +178,57 @@ GLuint matToTexture(cv::Mat &mat, GLenum minFilter, GLenum magFilter, GLenum wra
 	return textureID;
 }
 
-double convertX(double x, double width, double height){
-	double xmax_cam = imgOriginal.rows, ymax_cam = imgOriginal.cols;
+/**
+	\func convertit la coordonnée X de l'espace OpenCV vers l'espace OpengGL
+	\param x coordonnée à convertir
+	\param width largeur du rectangle
+	\return coordonnée convertie
+**/
+double convertX(double x, double width){
+	/*double xmax_cam = imgOriginal.rows, ymax_cam = imgOriginal.cols;
 	INFO("convert x = %f \n", ((((2*x*(xmax_cam/ymax_cam))/xmax_cam)-1)*(-1))-width);
-	return ((((2*x*(xmax_cam/ymax_cam))/xmax_cam)-1)*(-1))-width;
+	return ((((2*x*(xmax_cam/ymax_cam))/xmax_cam)-1)*(-1))-width;*/
+
+	INFO("convert x = %f \n", ((((2*x*((double)imgOriginal.rows/(double)imgOriginal.cols))/(double)imgOriginal.rows)-1)*(-1))-width);
+	return ((((2*x*((double)imgOriginal.rows/(double)imgOriginal.cols))/(double)imgOriginal.rows)-1)*(-1))-width;
 }
 
-double convertY(double y, double width, double height){
-	double xmax_cam = imgOriginal.rows, ymax_cam = imgOriginal.cols;
+/**
+	\func convertit la coordonnée Y de l'espace OpenCV vers l'espace OpengGL
+	\param y coordonnée à convertir
+	\param height hauteur du rectangle
+	\return coordonnée convertie
+**/
+double convertY(double y, double height){
+	/*double xmax_cam = imgOriginal.rows, ymax_cam = imgOriginal.cols;
 	INFO("convert y = %f \n", (((2*(ymax_cam-y*(ymax_cam/xmax_cam)))/ymax_cam)-1)-height);
-	return (((2*(ymax_cam-y*(ymax_cam/xmax_cam)))/ymax_cam)-1)-height;
+	return (((2*(ymax_cam-y*(ymax_cam/xmax_cam)))/ymax_cam)-1)-height;*/
+
+	INFO("convert y = %f \n", (((2*((double)imgOriginal.cols-y*((double)imgOriginal.cols/(double)imgOriginal.rows)))/(double)imgOriginal.cols)-1)-height);
+	return (((2*((double)imgOriginal.cols-y*((double)imgOriginal.cols/(double)imgOriginal.rows)))/(double)imgOriginal.cols)-1)-height;
 }
 
-double convertWidth(double toConvert){
-	return ((2*toConvert)/imgOriginal.rows)+0.1;
+/**
+	\func convertit la largeur de l'espace OpenCV vers l'espace OpengGL
+	\param width largeur à convertir
+	\return largeur convertie
+**/
+double convertWidth(double width){
+	return ((2*width)/imgOriginal.rows)+0.1;
 }
 
-double convertHeight(double toConvert){
-	return ((2*toConvert)/imgOriginal.cols)+0.1;
+/**
+	\func convertit la hauteur de l'espace OpenCV vers l'espace OpengGL
+	\param height hauteur à convertir
+	\return hauteur convertie
+**/
+double convertHeight(double height){
+	return ((2*height)/imgOriginal.cols)+0.1;
 }
 
+/**
+	\func détecte la couleur jaune sur les images de la caméra (l'objet le plus gros est retourné sous la forme d'un rectangle (x,y,w,h))
+**/
 void detectColor(){
 	int lowH = 10;
 	int highH = 30;
@@ -215,15 +280,17 @@ void detectColor(){
 
 	Scalar color(255,255,255);
 	//rectangle(imgOriginal, bounding_rect, Scalar(0,255,0),1, 8,0);
-	initSwordPosition(convertX(bounding_rect.x,convertWidth(bounding_rect.width)+0.2,convertHeight(bounding_rect.height)+0.2),
-							convertY(bounding_rect.y,convertWidth(bounding_rect.width)+0.2,convertHeight(bounding_rect.height)+0.2), 
+	initSwordPosition(convertX(bounding_rect.x,convertWidth(bounding_rect.width)+0.2),
+							convertY(bounding_rect.y,convertHeight(bounding_rect.height)+0.2), 
 							0.1,
 							convertWidth(bounding_rect.width)+0.2,
 							convertHeight(bounding_rect.height)+0.2);
 }
 
+/**
+	\func initialise tous les paramètres relatifs à OpenGL
+**/
 static void initGL(void){
-	initCV();
 	glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
@@ -233,13 +300,13 @@ static void initGL(void){
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	glimageLoadAndBind("hilt.png", &hiltText);
+	glimageLoadAndBind("img/hilt.png", &hiltText);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-	glimageLoadAndBind("blade.png", &bladeText);
+	glimageLoadAndBind("img/blade.png", &bladeText);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP);
 	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -248,21 +315,29 @@ static void initGL(void){
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
+/**
+	\func redimensionne la fenêtre
+	\param width largeur de la fenêtre
+	\param height hauteur de la fenêtre
+**/
 void reshape(int width, int height){
 	glViewport(0,0,(GLsizei)(width),(GLsizei)(height));
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0f, (GLfloat)width/height, 0.01f, 5000.0f);	
+	gluPerspective(60.0f, (GLfloat)width/height, 0.01f, 1000.0f);	
 	glMatrixMode(GL_MODELVIEW);
 }
 
+/**
+	\func dessine le manche du sabre laser sur un cube texturé
+**/
 static void drawHilt(){
 	glBindTexture(GL_TEXTURE_2D, hiltText);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-	glVertexPointer(3, GL_FLOAT, 5*sizeof(GLfloat), swordVBO);
-	glTexCoordPointer(2, GL_FLOAT, 5*sizeof(GLfloat), &(swordVBO[3]));
+	glVertexPointer(3, GL_FLOAT, 5*sizeof(GLfloat), hiltVBO);
+	glTexCoordPointer(2, GL_FLOAT, 5*sizeof(GLfloat), &(hiltVBO[3]));
 	
 	glDrawArrays(GL_QUADS, 0, 4);
 
@@ -270,6 +345,9 @@ static void drawHilt(){
 	glDisableClientState(GL_VERTEX_ARRAY);	
 }
 
+/**
+	\func dessine la lame du sabre laser sur un cube texturé
+**/
 static void drawBlade(){
 	glBindTexture(GL_TEXTURE_2D, bladeText);
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -284,6 +362,9 @@ static void drawBlade(){
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
+/**
+	dessine la capture de la caméra sur un cube texturé
+**/
 static void drawCam(){
 	detectColor();
 	camText = matToTexture(imgOriginal, GL_NEAREST, GL_NEAREST, GL_CLAMP);
@@ -298,6 +379,9 @@ static void drawCam(){
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
 
+/**
+	\func dessine l'univers OpenGL
+**/
 void display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();					
@@ -310,8 +394,13 @@ void display(void){
 	glutSwapBuffers();
 }
 
+/**
+	\n gère les entreés claviers
+**/
 static void keyboard(unsigned char key, int x, int y){
-	if (key == 27) exit(0);
+	switch(key){
+		case 27: exit(0); break;
+	}
 }
 
 int main(int argc, char **argv){
@@ -320,6 +409,7 @@ int main(int argc, char **argv){
 	glutInitWindowSize (500, 500); 
 	glutInitWindowPosition (100, 100); 
 	glutCreateWindow (argv[0]);
+	initCV();
 	initGL();  
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
